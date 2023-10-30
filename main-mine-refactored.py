fac0e233c22f431aac326be2f6e83034c484c97b
Adam Knowles <1413836+Pharkie@users.noreply.github.com>
2023-10-30 14:31:20 +0000

    first mostly working vesion

--- main-mine-refactored.py
+++ main-mine-refactored.py
@@ -21,18 +21,16 @@ from max7219_matrix import max7219_matrix
 CS_PIN = 5
 
 max7219_eyes = max7219_matrix(machine.SPI(0), machine.Pin(CS_PIN, machine.Pin.OUT, True))
-print(f"max7219_eyes: {max7219_eyes}")
 
 def load_anims(file_name):
     """Load animations from json file"""
     data = {}
+    
     try:
         with open(file_name, encoding='utf-8') as infile:
             data = json.load(infile)
     except FileNotFoundError as err:
         print('Oops problem loading JSON! File not found.')
-    except json.JSONDecodeError as err:
-        print('Oops problem loading JSON! Invalid JSON data.')
 
     return data
 
@@ -41,20 +39,13 @@ def anim_runner(anims, font):
     for anim in anims:
         left = anim.get("l")
         right = anim.get("r")
-        if left is not None and right is not None:
-            max7219_eyes.show_char(font[left], font[right])
-        brightness = anim.get("bl")
-        if brightness is not None:
-            max7219_eyes.set_brightness(brightness)
-        delay = anim.get("d")
-        if delay is not None:
-            time.sleep(delay)
+        if left is not None:
+            max7219_eyes.show_char(font[left], None)
+        if right is not None:
+            max7219_eyes.show_char(None, font[right])
+        time.sleep(0.1)
 
-def show_char(left, right):
-    """Show character"""
-    max7219_eyes.show_char(left,right)
-
-def scroll_message(font, message='hello', delay=0.05):
+def scroll_message(font, message='hello', delay=0.1):
     """Scroll message"""
     left_message = '   ' + message
     right_message = message + '   '
@@ -83,33 +74,31 @@ def scroll_message(font, message='hello', delay=0.05):
 
 def main():
     """Run main program"""
-    print(f"Running main()")
+    print("Running main()")
     anims = load_anims('eyes_ani.json')
-
-    # max7219_eyes.show_char(matrix_fonts.eyes['ghost1'], matrix_fonts.eyes['ghost1'])
 
     while True:
         show_char(matrix_fonts.eyes['ghost1'], matrix_fonts.eyes['ghost1'])
         time.sleep(1)
         anim_runner(anims['stareAndBlink'],matrix_fonts.eyes)
-        scroll_message(matrix_fonts.textFont1, ' Spooky ', 0.05)
-        anim_runner(anims['growEyes'],matrix_fonts.eyes)
-        anim_runner(anims['roll'],matrix_fonts.eyes)
-        anim_runner(anims['downLeftABit'],matrix_fonts.eyes)
-        anim_runner(anims['stareAndBlink'],matrix_fonts.eyes)
-        anim_runner(anims['downRightABit'],matrix_fonts.eyes)
-        anim_runner(anims['stareAndBlink'],matrix_fonts.eyes)
-        scroll_message(matrix_fonts.textFont1, ' Trick or Treat ')
-        anim_runner(anims['roll'],matrix_fonts.eyes)
-        anim_runner(anims['stareAndBlink'],matrix_fonts.eyes)
-        anim_runner(anims['stareAndBlink'],matrix_fonts.eyes)
-        anim_runner(anims['growEyes'],matrix_fonts.eyes)
-        anim_runner(anims['roll'],matrix_fonts.eyes)
-        anim_runner(anims['ghosts1'],matrix_fonts.eyes)
-        anim_runner(anims['stareAndBlink'],matrix_fonts.eyes)
-        anim_runner(anims['winkLeft'],matrix_fonts.eyes)
-        anim_runner(anims['winkRight'],matrix_fonts.eyes)
-        anim_runner(anims['stareAndBlink'],matrix_fonts.eyes)
+        scroll_message(matrix_fonts.textFont1, ' Spooky ')
+        # anim_runner(anims['growEyes'],matrix_fonts.eyes)
+        # anim_runner(anims['roll'],matrix_fonts.eyes)
+        # anim_runner(anims['downLeftABit'],matrix_fonts.eyes)
+        # anim_runner(anims['stareAndBlink'],matrix_fonts.eyes)
+        # anim_runner(anims['downRightABit'],matrix_fonts.eyes)
+        # anim_runner(anims['stareAndBlink'],matrix_fonts.eyes)
+        # scroll_message(matrix_fonts.textFont1, ' Trick or Treat ')
+        # anim_runner(anims['roll'],matrix_fonts.eyes)
+        # anim_runner(anims['stareAndBlink'],matrix_fonts.eyes)
+        # anim_runner(anims['stareAndBlink'],matrix_fonts.eyes)
+        # anim_runner(anims['growEyes'],matrix_fonts.eyes)
+        # anim_runner(anims['roll'],matrix_fonts.eyes)
+        # anim_runner(anims['ghosts1'],matrix_fonts.eyes)
+        # anim_runner(anims['stareAndBlink'],matrix_fonts.eyes)
+        # anim_runner(anims['winkLeft'],matrix_fonts.eyes)
+        # anim_runner(anims['winkRight'],matrix_fonts.eyes)
+        # anim_runner(anims['stareAndBlink'],matrix_fonts.eyes)
 
 if __name__ == "__main__":
     main()
