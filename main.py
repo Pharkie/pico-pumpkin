f6e21c9eb185fdb998a4a5bf37fb91e585fcfee0
Adam Knowles <1413836+Pharkie@users.noreply.github.com>
2024-10-22 21:04:30 +0100

    Mark & Alice working (fixed RGB LED order)

--- main.py
+++ main.py
@@ -20,20 +20,36 @@ import matrix_fonts
 from max7219_matrix import max7219_matrix
 
 # User-settable variables
-SPI_BUS = 1  # Use SPI(1) for ESP32-C3
-CLK_PIN = 6
-DIN_PIN = 7
-CS_PIN = 10
+
+# Pico specific pins
+SPI_BUS = 0  # Use SPI(0) for Raspberry Pi Pico
+CLK_PIN = 2  # CLK / SCK
+DIN_PIN = 3  # DIN / MOSI / TX
+CS_PIN = 1   # CS
 
 MAX_BRIGHT = 2  # Set global max brightness 0-15
 
 RGB_LED_CONNECTED = True  # Set to False if RGB LED is not connected
 # These pins aren't used if RGB_LED_CONNECTED is False
-RED_PIN = 1
-GREEN_PIN = 2
-BLUE_PIN = 3
+RED_PIN = 14
+GREEN_PIN = 15
+BLUE_PIN = 16
 
-DEBUG = False # Set to True to log messages to log.txt
+# ESP32-C3 specific pins
+# SPI_BUS = 1  # Use SPI(1) for ESP32-C3
+# CLK_PIN = 6
+# DIN_PIN = 7
+# CS_PIN = 10
+
+# MAX_BRIGHT = 2  # Set global max brightness 0-15
+
+# RGB_LED_CONNECTED = True  # Set to False if RGB LED is not connected
+# # These pins aren't used if RGB_LED_CONNECTED is False
+# RED_PIN = 1
+# GREEN_PIN = 2
+# BLUE_PIN = 3
+
+DEBUG = True # Set to True to log messages to log.txt
 
 # Custom logging function
 # Logs messages to a file with a timestamp
@@ -73,13 +89,11 @@ if RGB_LED_CONNECTED:
     COLOUR_RED = (255, 0, 0)
     COLOUR_GREEN = (0, 255, 0)
     COLOUR_MAGENTA = (255, 0, 255)
-    COLOUR_PINK = (255, 192, 203)
 
     LED_COLOURS = [
         COLOUR_RED,
         COLOUR_GREEN,
         COLOUR_MAGENTA,
-        COLOUR_PINK
     ]
 
     red = machine.Pin(RED_PIN, machine.Pin.OUT)
@@ -193,7 +207,7 @@ def main():
         show_char(matrix_fonts.eyes['ghost1'], matrix_fonts.eyes['ghost2'])
         time.sleep(1)
         anim_runner(anims_JSON, "stareAndBlink", matrix_fonts.eyes)
-        scroll_message(matrix_fonts.textFont1, " Tessa's Pumpkin ")
+        scroll_message(matrix_fonts.textFont1, " Mark & Alice's Pumpkin ")
 
         show_char(matrix_fonts.shapes['heart1'], matrix_fonts.shapes['heart2'])
         time.sleep(0.5)
@@ -207,9 +221,7 @@ def main():
         scroll_message(
             matrix_fonts.textFont1,
             " Double, double toil and trouble; " +
-            "Fire burn and caldron bubble. " +
-            "Fillet of a fenny snake, " +
-            "In the caldron boil and bake ",
+            "Fire burn and caldron bubble. "
             0.03
         )
         
@@ -232,15 +244,15 @@ def main():
         anim_runner(anims_JSON, "stareAndBlink", matrix_fonts.eyes)
         anim_runner(anims_JSON, "growEyes", matrix_fonts.eyes)
 
-        scroll_message(
-            matrix_fonts.textFont1,
-            " Come, you spirits " +
-            "That tend on mortal thoughts! Unsex me here, " +
-            "And fill me from the crown to the toe top full " +
-            "Of direst cruelty; make thick my blood, " +
-            "Stop up the access and passage to remorse ",
-            0.03
-        )
+        # scroll_message(
+        #     matrix_fonts.textFont1,
+        #     " Come, you spirits " +
+        #     "That tend on mortal thoughts! Unsex me here, " +
+        #     "And fill me from the crown to the toe top full " +
+        #     "Of direst cruelty; make thick my blood, " +
+        #     "Stop up the access and passage to remorse ",
+        #     0.03
+        # )
 
         anim_runner(anims_JSON, "stareAndBlink", matrix_fonts.eyes)
         anim_runner(anims_JSON, "winkRight", matrix_fonts.eyes)
---
+++ test_led.py
@@ -0,0 +1,38 @@
+import machine
+import time
+
+# Define GPIO pins for the RGB LED
+RED_PIN = 14
+GREEN_PIN = 15
+BLUE_PIN = 16
+
+# Initialize the pins
+red = machine.Pin(RED_PIN, machine.Pin.OUT)
+green = machine.Pin(GREEN_PIN, machine.Pin.OUT)
+blue = machine.Pin(BLUE_PIN, machine.Pin.OUT)
+
+def set_rgb_color(r, g, b):
+    """Set the color of the RGB LED"""
+    red.value(not r)
+    green.value(not g)
+    blue.value(not b)
+
+def cycle_rgb_led():
+    """Cycle the RGB LED through red, green, and blue colors"""
+    while True:
+        # Red
+        print("Red")
+        set_rgb_color(1, 0, 0)
+        time.sleep(1)
+        # Green
+        print("Green")
+        set_rgb_color(0, 1, 0)
+        time.sleep(1)
+        # Blue
+        print("Blue")
+        set_rgb_color(0, 0, 1)
+        time.sleep(1)
+
+# Run the test
+if __name__ == "__main__":
+    cycle_rgb_led()
